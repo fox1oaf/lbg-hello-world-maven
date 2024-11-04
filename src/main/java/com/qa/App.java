614da66c7a6a801d8a5f51776e3d4faaec855b37
fox1oaf <makifoxloaf@gmail.com>
2024-11-04 15:11:44 +0000

    mini act

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,6 +13,7 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
+        System.out.println(sayHelloToSomeone("Kayla"));
         System.out.println(sayGoodbye());
     }
 
