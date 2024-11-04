d964e117f9f53a6c0213329e1fd64106fa77a1e7
Foxloaf <132841929+fox1oaf@users.noreply.github.com>
2024-11-04 16:16:41 +0000

    Update App.java

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayHelloToSomeone("Kayla123"));
+        System.out.println(sayHelloToSomeone("Joey"));
         System.out.println(sayGoodbye());
     }
 
